b27ba9c244dc522fd7dc56209d899b62c8d37164
CataReygadasG <catafranrey@gmail.com>
2023-08-15 10:04:07 -0400

    cambio

--- src/componentes/ItemDetail.jsx
+++ src/componentes/ItemDetail.jsx
@@ -2,11 +2,6 @@ import ItemCount from "./ItemCount"
 function ItemDetail({joya})  {
   
   return(
-   <article className="box">
-  Catalogo de joyas
-  <h3>{joya.name}</h3>
-  <h4> ${joya.price} </h4>
-</article> 
 <ItemCount joya={joya} />
 );
 
